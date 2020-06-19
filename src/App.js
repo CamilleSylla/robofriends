1d585318135b84e8d9183ec6d5aa8d07c1b2d06d
CamilleSylla <camillesylla.web@gmail.com>
2020-06-19 04:22:41 +0200

    compose some more style with React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './searchBox';
-import { robots } from './robots';
+import Scroll from './Scroll';
 import './App.css';
 
 
@@ -9,9 +9,19 @@ class App extends React.Component{
     constructor () {
         super()
         this.state = {
-            robots : robots,
+            robots : [],
             searchfieald : ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> {
+            return response.json();
+        })
+        .then(users=> {
+            this.setState({robots : users})
+        })
     }
 
     onSearchChange = (event) => {
@@ -27,7 +37,9 @@ class App extends React.Component{
             <div class ='tc'>
                 <h1 className ='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
                 <CardList robots={filteredRobots} />
+                </Scroll>
             </div>
         );
     }
